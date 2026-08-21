dfdf5f760fd40fddbda13506e8b7336f46f4624b
Paul <paaul.rbn@gmail.com>
2026-08-21 21:09:42 +0200

    Revert "feat: add settings page with persistent store"
    This reverts commit 8b7e8b740b155cb932d3863d6052492f82029595.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -266,7 +266,6 @@ fn play_video(url: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_opener::init())
         .manage(DownloadState(Mutex::new(HashMap::new())))
         .invoke_handler(tauri::generate_handler![play_video, download_video, get_config, fetch_radarr_movies, cancel_download])
